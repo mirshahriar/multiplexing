940c72a692c724345d5e6a99f909dace04457eb5
Mir Shahriar Sabuj <shahriar@Mirs-MacBook-Pro.local>
2023-11-05 16:53:31 +0600

    updated log

--- grpc/server.go
+++ grpc/server.go
@@ -3,7 +3,6 @@ package grpc
 import (
 	"context"
 	"fmt"
-
 	"github.com/mirshahriar/multiplexing/grpc/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -12,7 +11,7 @@ import (
 type grpcServer struct{}
 
 func (s *grpcServer) EchoMessage(ctx context.Context, req *proto.EchoRequest) (*proto.EchoResponse, error) {
-	fmt.Println("echo message", req.Message)
+	fmt.Println("gRPC server request received")
 	return &proto.EchoResponse{Message: fmt.Sprintf("echo %s from grpc!", req.Message)}, nil
 }
 
--- http/server.go
+++ http/server.go
@@ -8,7 +8,7 @@ import (
 func NewHTTPServer() *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("HTTP server received request")
+		fmt.Println("HTTP server request received")
 		_, _ = w.Write([]byte("echo from HTTP!\n"))
 	})
 
